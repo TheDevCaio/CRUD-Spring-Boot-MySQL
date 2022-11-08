05d2deca917396ff3e75e1a4c55455cf2fa5d063
Caio Dev <110746976+TheDevCaio@users.noreply.github.com>
2022-11-08 20:23:32 -0300

    Painel de cadastro.
    Subindo painel de cadastro.

---
+++ src/test/java/com/web/PainelSpring/PainelSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.PainelSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainelSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
